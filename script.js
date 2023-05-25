5b81da6213ff46d3b73412d060fafc268929bc7f
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2023-05-25 08:15:07 +0530

    Update2 script.js

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 const myForm=document.querySelector('#my-form')
 const nameInput=document.querySelector('#name')
 const emailInput=document.querySelector('#email')
+const phoneInput=document.querySelector('#phone')
+const userList=document.querySelector('#users')
 
 myForm.addEventListener('submit',onSubmit)
 function onSubmit(e)
 {
     e.preventDefault()
-    if(nameInput.value=="" || emailInput.value=="")
+    if(nameInput.value=="" || emailInput.value=="" || phoneInput.value=="")
     {
         alert('Please Enter Details!!!')
     }
     else
     {   let myObj={
         name:nameInput.value,
-        email:emailInput.value
+        email:emailInput.value,
+        phone:phoneInput.value
         }
         let myObj_serialised=JSON.stringify(myObj)
-        localStorage.setItem('obj',myObj_serialised)
+        localStorage.setItem(nameInput.value,myObj_serialised)
         document.querySelector('#head').textContent="USER ADDED"
+        const li=document.createElement('li')
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value} : ${phoneInput.value}`))
+        userList.appendChild(li)
         
         nameInput.value=""
         emailInput.value=""
